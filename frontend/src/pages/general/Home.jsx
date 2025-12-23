442b2140607af64ea7f61f7fae8d8516804e3902
deep <your_github_deepikas19998@gmail.com>
2025-12-24 03:44:59 +0530

    Update Saved component and API fetch

--- frontend/src/pages/general/Home.jsx
+++ frontend/src/pages/general/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-import '../styles/reels.css'
-import ReelFeed from "../components/ReelFeed";
+import '../../styles/reels.css'
+import ReelFeed from "../../components/ReelFeed";
 
 
 
